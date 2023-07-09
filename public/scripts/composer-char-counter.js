20998dbc366306bac944564f6441b32d7d5c89a0
carolineolagunju <olamidejr21@gmail.com>
2023-07-09 02:25:01 -0600

    implement character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
-  $(".new-tweet textarea").on("event", () => {
 
+  //targeting textarea element and listening to keypress
+  $("textarea").keypress(function() {
+
+    //fetching the current length of the text area
+    let currentLength = $(this).val().length + 1;
+    const maxLength = 140;
+    const remainingLength = maxLength - currentLength;
+
+    //targeting the counter
+    const targetElement = $(".counter");
+
+    //checking if the length of char is <= the specified maxLength
+    if (currentLength <= maxLength) {
+      targetElement.text(remainingLength);
+    } else if (currentLength > maxLength) {
+      targetElement.text(remainingLength).css("color", "red");
+    }
   });
-
 });
