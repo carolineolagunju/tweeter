6c750bfe2650391133c86cb732278cea8def8ee7
carolineolagunju <olamidejr21@gmail.com>
2023-07-12 14:51:42 -0600

    implement function createTweetElement

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,3 +3,49 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
+$(document).ready(function() {
+  //The tweet object
+  const tweetData =  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+  }
+
+    //function to generate tweet <article> that returns the HTML structure
+  const createTweetElement = function(tweetObj) {
+    //create the <article> (html structure) for the tweet
+    const $tweet = $(`<article class="tweet">
+          <header>
+            <span>
+              <img src=${tweetObj.user.avatars}>
+              <span>${tweetObj.user.name}</span>
+          </span>
+            <span class="username">${tweetObj.user.handle}</span>
+          </header>
+          <p>${tweetObj.content.text}</p>
+          <hr>
+          <footer class="footer">
+            <span>${tweetObj.created_at}</span>
+            <div class="icons">
+              <i class="fa-solid fa-flag"></i>
+              <i class="fa-sharp fa-solid fa-retweet"></i>
+              <i class="fa-solid fa-heart"></i>
+            </div>
+          </footer>
+        </article>`);
+  
+        return $tweet;
+  }
+
+  //calling function createTweetElement and passing tweetData as an argument
+  const $tweet = createTweetElement(tweetData);
+  //append the html structure to id tweet-container from index.html
+  $("#tweet-container").append($tweet);
+});
